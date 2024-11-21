d589a06c61dee6c9cc28602b5075ac31b6ceabc1
Vijay Kumawat <vijaykumawat2908@gmail.com>
2024-11-21 21:53:35 +0530

    updated package

--- src/App.js
+++ src/App.js
@@ -18,12 +18,16 @@ function App() {
           containerStyle={{ width: "325px" }}
           placeholder="Select date"
           selectionType="single"
+          dateFormat="YY MM"
+          viewFormat="YYYY MMM DD"
         />
         <p className="heading">Date Range Selection (Absolute)</p>
         <DatePicker
           containerStyle={{ width: "325px" }}
           placeholder="Select date"
           selectionType="range"
+                 dateFormat="YY MM"
+          viewFormat="YYYY MMM DD"
         />
         <p className="heading">Date Range Selection (Relative)</p>
         <DatePicker
